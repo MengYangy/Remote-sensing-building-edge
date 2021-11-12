0ac4a645545b669bfebcea7cc5b726591e048268
MengYangy <mengyy1029@gmail.com>
2021-11-12 14:42:41 +0800

    CONFIG

---
+++ config.py
@@ -0,0 +1,22 @@
+Filter_Size = 16
+Batch_Size = 2
+
+# 输入图像大小
+Image_Size = 128
+BUFFER_Size = 400
+
+# 阈值
+THRESHOLD = 0
+
+# train
+IMAGE_PATH = 'D:/dataset/newedge/train/image'
+LABEL_PATH = 'D:/dataset/newedge/train/label'
+EDGE_LABEL_PATH = 'D:/dataset/newedge/train/edgelabel'
+
+# val
+VAL_IMAGE_PATH = 'D:/dataset/newedge/val/image'
+VAL_LABEL_PATH = 'D:/dataset/newedge/val/label'
+VAL_EDGELABEL_PATH = 'D:/dataset/newedge/val/edgelabel'
+
+# pred_path
+PRED_PATH = r'D:\dataset\newedge\val\image\val_1015.png'
